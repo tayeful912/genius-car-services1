c4be856b6ae708ededfb217566d962db8fc38261
tayeful912 <kazitayefulislam12@gmail.com>
2022-04-14 12:42:00 +0600

    login form added in login component form bootstrap

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from './Pages/Home/Home/Home';
 import Header from './Pages/Shared/Header/Header';
 import Footer from './Pages/Shared/Footer/Footer';
 import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
+import NotFound from './Pages/Shared/NotFound/NotFound';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path='/home' element={<Home></Home>}> </Route>
         <Route path='/service/:serviceId' element={<ServiceDetail></ServiceDetail>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
 
 
       </Routes>
---
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+
+            <h1>Tata Bye Bye Page to pawa jai nai</h1>
+
+        </div>
+    );
+};
+
+export default NotFound;
